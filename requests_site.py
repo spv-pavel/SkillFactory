92fff330e051cd5d9d14da6fe468e3ed7eea4245
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-06 16:39:58 +0300

    requests

---
+++ requests_site.py
@@ -0,0 +1,5 @@
+import requests
+
+# r = requests.get(
+#     'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html')  # делаем запрос на сервер по переданному адресу
+# print(r.content)
