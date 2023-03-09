7eb18c4c0c2ff4cf3fafd46a04b17d0b61573c33
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-09 16:07:54 +0300

    @bot.message_handler

--- practice_2023/Itvoz_EchoBoyBot.py
+++ practice_2023/Itvoz_EchoBoyBot.py
@@ -3,15 +3,23 @@ import telebot
 TOKEN = "5866957629:AAGDq8FDrVVlyZWrdu8UNKM9WX2I6xBBgFU"
 
 bot = telebot.TeleBot(TOKEN)
-# bot.polling(none_stop=True)
+
 
 # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    pass
+    bot.send_message(message.chat.id, 'Команда')
 
 
 # Обрабатывается все документы и аудиозаписи
-@bot.message_handler(content_types=['document', 'audio'])
+@bot.message_handler(content_types=['document', 'audio', 'text', 'voice'])
 def handle_docs_audio(message):
-    pass
+    bot.send_message(message.chat.id, 'Ответ')
+
+
+@bot.message_handler(content_types=['photo'])
+def say_lmao(message: telebot.types.Message):
+    bot.reply_to(message, 'Nice meme XDD')
+
+
+bot.polling(none_stop=True)
