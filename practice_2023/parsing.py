dddc047d64c45f63720d326468ea2f1233f722a1
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-10 11:40:58 +0300

    first cod

---
+++ practice_2023/parsing.py
@@ -0,0 +1,15 @@
+import requests  # импортируем наш знакомый модуль
+import lxml.html
+from lxml import etree
+
+html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
+
+tree = lxml.html.document_fromstring(html)
+title = tree.xpath(
+    '/html/head/title/text()')
+# забираем текст тега <title> из тега <head>, который лежит в свою очередь внутри тега <html>
+# (в этом невидимом теге <head> у нас хранится основная информация о страничке, её название и инструкции по отображению
+# в браузере)
+
+print(title)  # выводим полученный заголовок страницы
+print(tree)
